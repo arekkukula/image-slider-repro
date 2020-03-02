41022344ab71900c1081b37842b3671ec91b7f9b
Arek Kukula <arek.kukula@gmail.com>
2020-03-02 12:23:54 +0100

    Provide minimal repro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgImageSliderModule } from "ng-image-slider"
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgImageSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
